4522d4b6ef41602e8f0a55561213c3ff3a83118c
sowmik-sec <sowmik.sec@gmail.com>
2024-11-30 20:26:51 +0600

    Middleware and custom middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
-
+import logger from "./middlewares/logger";
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   //   devTools: true,
 });
 

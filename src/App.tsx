e6fa0f9d51c730dc67a1b26360654b0434e8003f
sowmik-sec <sowmik.sec@gmail.com>
2024-11-30 19:47:01 +0600

    Connect component with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+
 function App() {
+  const { count } = useSelector((state: RootState) => state.counter);
   return (
     <>
-      <h1 className="bg-red-300">Tech net</h1>
+      <div className="m-5 flex">
+        <button className="border border-purple-500 px-3 py-2 rounded-md">
+          increment
+        </button>
+        <div className="mx-5 py-2">{count}</div>
+        <button className="border border-purple-500 px-3 py-2 rounded-md">
+          decrement
+        </button>
+      </div>
     </>
   );
 }
--- src/redux/features/counter/counterSlice.ts
+++ src/redux/features/counter/counterSlice.ts
@@ -11,5 +11,10 @@ const counterSlice = createSlice({
     increment: (state) => {
       state.count += 1;
     },
+    decrement: (state) => {
+      state.count -= 1;
+    },
   },
 });
+
+export default counterSlice.reducer;

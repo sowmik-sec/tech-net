fee0f88b09a84431c8ba6a3a86ccc47379054fba
sowmik-sec <sowmik.sec@gmail.com>
2024-11-30 18:36:04 +0600

    Setup redux store

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../page/Home";
 import Login from "../page/Login";
+import NotFound from "../page/NotFound";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-  {
-    path: "/home",
-    element: <Home />,
-  },
+
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
